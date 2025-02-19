72173da652d9a0bbd5ff530e40ac0aa77bcd0650
Georges FARDISSI <fardissi.georges@gmail.com>
2025-02-19 16:50:34 +0200

    Last Commit

--- Lab05.API/Program.cs
+++ Lab05.API/Program.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
